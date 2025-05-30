6d7efdc99c73fc5faa48e78041d6a0af72354235
Kaitlyn Le <xkait.le1221@gmail.com>
2025-05-30 16:08:14 -0700

    Star class which extends the super Celestial, calling the update draw and type method. Returning nothing for update because the sun is still, draws the sun, and gets the type of the subclass which is Star.

--- src/GalaxyProject/Star.java
+++ src/GalaxyProject/Star.java
@@ -1,6 +1,30 @@
-
 package GalaxyProject;
 
-public class Star {
-    //TESTING
+import java.awt.*;
+
+// A Star class extends the superclass Celestial
+public class Star extends Celestial {
+    
+    // A Star constructor which calls the super for the x and y coordinates, with mass 1500 and size 30
+    public Star(double x, double y) {
+        super(x, y, 1500, 30);
+    }
+    
+    // A update method that returns nothing because in this case the sun (A dwarf star) is not moving
+    public void update() {
+        return;
+    }
+    
+    // An overriden draw method which draws out a circular yellow sun
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval((int)(getX() - getSize() / 2), (int)(getY() - getSize() / 2), getSize(), getSize());
+    }
+    
+    // An overriden getType method which returns the String of the type "Star"
+    @Override
+    public String getType() {
+        return "Star";
+    }
 }
