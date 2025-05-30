4f408f7ee34a25d5ad49dbfdd08a4f9c25163793
Kaitlyn Le <xkait.le1221@gmail.com>
2025-05-30 00:35:05 -0700

    Celestial class for the x and y locations, size, and mass. And update and draw methods which will be overriden. Setter felt a bit redundant so I didn't include it but if you want to we can add it in. Also was thinking that if you wanted to look for the grid's location would you want to add a toString method to print out the coordinates for it?

--- src/GalaxyProject/Celestial.java
+++ src/GalaxyProject/Celestial.java
@@ -1,6 +1,42 @@
-
 package GalaxyProject;
 
+import java.awt.*;
+
 public class Celestial {
-    //TESTING GITHUB FILES
+    private double x;
+    private double y;
+    private double mass;
+    private int size;
+    
+    public Celestial(double x, double y, double mass, int size) {
+        this.x = x;
+        this.y = y;
+        this.mass = mass;
+        this.size = size;
+    }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+    public double getMass() {
+        return mass;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+    
+    public void update() {
+        
+    }
+    
+    public void draw(Graphics g) {
+        
+    }
+    
 }
