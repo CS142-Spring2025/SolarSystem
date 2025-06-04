0490c22696c473ceab6732c60ebc91634765e285
Emma Dennis <119965766+EmmaBlueJeans@users.noreply.github.com>
2025-06-03 17:50:13 -0700

    changed BlackHole event horizon and the collision check in Simulation.  Now when an object passes the red ring in the black hole it is sucked in

--- src/GalaxyProjectFinal/GalaxySimulation.java
+++ src/GalaxyProjectFinal/GalaxySimulation.java
@@ -24,12 +24,13 @@ import java.util.List;
         return objects;
     }
 
-        public void update() {   //loops through all objects and calls their update().
-            for (Celestial obj : objects) {
-                obj.update();
+    public void update() {   //loops through all objects and calls their update().
+            
+        for (Celestial obj : objects) {
+            obj.update();
          
-            }
-            // Handle collisions - simple version
+        }
+            //check collisions and black hole pull
         for (int i = 0; i < objects.size(); i++) {
             Celestial a = objects.get(i);
             if (a.isDestroyed()) continue;
@@ -39,30 +40,42 @@ import java.util.List;
                 if (b.isDestroyed()) continue;
 
                 // Check collisions (using existing helper methods)
+                //asteroid-asteroid collision
                 if (a instanceof Asteroid && b instanceof Asteroid) {
-                    if (((Asteroid) a).collidesWith(b)) {
-                        ((Asteroid) a).handleCollision((Asteroid) b);
+                    Asteroid aAst = (Asteroid) a;
+                    Asteroid bAst = (Asteroid) b;
+                    if (aAst.collidesWith(bAst)) {
+                        aAst.handleCollision(bAst);
                     }
                 } else if (a instanceof Comet && b instanceof Comet) {
-                    if (((Comet) a).collidesWith(b)) {
-                        ((Comet) a).handleCollision((Comet) b);
+                    Comet aCom = (Comet) a;
+                    Comet bCom = (Comet) b;
+                    if (aCom.collidesWith(bCom)) {
+                        aCom.handleCollision(bCom);
                     }
-                } else if (a instanceof Comet && b instanceof Asteroid) {
-                    if (((Comet) a).collidesWith(b)) {
-                        ((Comet) a).handleCollision((Asteroid) b);
+                } else if ((a instanceof Comet && b instanceof Asteroid) || (a instanceof Asteroid && b instanceof Comet)) {
+                    Comet c = (a instanceof Comet) ? (Comet) a : (Comet) b;
+                    Asteroid m = (a instanceof Asteroid) ? (Asteroid) a : (Asteroid) b;
+                    if (c.collidesWith(m)) {
+                        c.handleCollision(m);
                     }
-                } else if (a instanceof BlackHole) {
+                }
+               
+                
+                ////black hole gravitational pull
+                
+                if (a instanceof BlackHole) {
                     ((BlackHole) a).applyGravitationalPull(b);
-                } else if (b instanceof BlackHole) {
+                }
+                if (b instanceof BlackHole) {
                     ((BlackHole) b).applyGravitationalPull(a);
                 }
-            }
-        }
-
+            }    
+        }      
+               
         // Remove destroyed objects
         objects.removeIf(Celestial::isDestroyed);
     }
 }
 
 
-// Simulation: collisions, add objects, 
